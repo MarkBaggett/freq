04d6169c534dce63e0ba2f55a67e791c09fc8dbb
Dom Hall <dominiczy.hall@gmail.com>
2019-03-19 00:54:27 +0000

    Cleaned up variable names in probability function

--- freq.py
+++ freq.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 
 class node():
     """ Assigning a weight actually adds that value.  It doesn't set it to that value.
-        For example node['c'] = 5    increases the value in node by 5   
+        For example node['c'] = 5    increases the value in node by 5
 """
 
     def __init__(self,parent):
@@ -23,13 +23,13 @@ class node():
         if self.parent.ignore_case and (key.islower() or key.isupper()):
             return  self._pairs[key.lower()] + self._pairs[key.upper()]
         else:
-            return self._pairs[key]            
-         
+            return self._pairs[key]
+
     def __setitem__(self,key,value):
         self._dirtycount = True
         self._pairs.update([key]*value)
 
-    @property   
+    @property
     def count(self):
         if self._dirtycount:
            self._cachecount = sum(self._pairs.values())
@@ -42,10 +42,10 @@ class FreqCounter(dict):
         self.ignore_case = False
         self.ignorechars = ""
         self.verbose = "verbose" in kwargs
-        
+
     def __getitem__(self,key):
         return self._table[key]
-        
+
     def __iter__(self):
         return iter(self._table)
 
@@ -56,7 +56,7 @@ class FreqCounter(dict):
         serial = []
         for key,val in self._table.items():
             serial.append( (key, list(val._pairs.items())) )
-        return json.dumps((self.ignore_case, self.ignorechars, serial)) 
+        return json.dumps((self.ignore_case, self.ignorechars, serial))
 
     def fromJSON(self,jsondata):
         args = json.loads(jsondata)
@@ -67,7 +67,7 @@ class FreqCounter(dict):
                 self._table[outerkey] = node(self)
                 for letter,count in val:
                    self._table[outerkey][letter] = count
-        
+
     def tally_str(self,line,weight=1):
         """tally_str() accepts two parameters.  A string and optionally you can specify a weight."""
         allpairs = re.findall(r"..", line)
@@ -75,23 +75,21 @@ class FreqCounter(dict):
         for eachpair in allpairs:
             self[eachpair[0]][eachpair[1]] = weight
 
-  
+
     def probability(self,line):
-        """This function tells you how probable the letter combination provided is giving the character frequencies. Ex .probability("test") returns ~%35 """
+        """This function tells you how probable the letter combination provided is giving the character frequencies. Ex .probability("test") returns (8.9652, 8.2914) """
         allpairs = re.findall(r"..", line)
         allpairs.extend(re.findall(r"..",line[1:]))
         if self.verbose: print("All pairs: {0}".format(allpairs))
-       #probability 1 is the average probability
         probs = []
         for eachpair in allpairs:
             if (eachpair[0] not in self.ignorechars) and (eachpair[1] not in self.ignorechars):
                 probs.append(self._probability(eachpair))
                 if self.verbose: print ("Probability of {0}: {1}".format(eachpair,probs))
-        probability1 = sum(probs)/ len(probs) * 100
+        average_probability = sum(probs)/ len(probs) * 100
         if self.verbose:
-            print("PROBABILITY 1: Average probability as percentage {0} \n\n".format(probability1))
-        #probability2 is the Total word probabilty
-        totl1 = 0 
+            print("Average Probability: Average probability as percentage {0} \n\n".format(average_probability))
+        totl1 = 0
         totl2 = 0
         for eachpair in allpairs:
              l1 = l2 = 0
@@ -104,13 +102,13 @@ class FreqCounter(dict):
                      l2 += self[eachpair[0].swapcase()][eachpair[1]]
                  totl1 += l1
                  totl2 += l2
-                 if self.verbose: print("Letter1:{0} Letter2:{1}  - This pair {2}:{3} {4}:{5}".format(totl1,totl2, eachpair[0],l1,eachpair[1],l2))          
+                 if self.verbose: print("Letter1:{0} Letter2:{1}  - This pair {2}:{3} {4}:{5}".format(totl1,totl2, eachpair[0],l1,eachpair[1],l2))
         if (totl1 == 0) or (totl2 == 0):
-            probability2 = 0
+            total_word_probability = 0
         else:
-            probability2 = totl2/totl1 * 100
-        if self.verbose: print("PROBABILITY 2: {0} /{1} = {2}".format(totl2, totl1, probability2))
-        return round(probability1,4),round(probability2,4)
+            total_word_probability = totl2/totl1 * 100
+        if self.verbose: print("Total Word Probability: {0} /{1} = {2}".format(totl2, totl1, total_word_probability))
+        return round(average_probability,4),round(total_word_probability,4)
 
     def _probability(self,twoletters):
         if self.ignore_case and (self[twoletters[0]].count == 0 and self[twoletters[0].swapcase()].count == 0):
@@ -138,7 +136,7 @@ class FreqCounter(dict):
 
     def load(self,filename):
         try:
-            file_handle =  open(filename,"rb") 
+            file_handle =  open(filename,"rb")
             self.fromJSON(file_handle.read().decode("latin1"))
             file_handle.close()
         except Exception as e:
@@ -190,7 +188,7 @@ if __name__ == "__main__":
 
     if args.printtable: fc.printtable()
     if args.normal: fc.tally_str(args.normal, args.weight)
-    if args.toggle_case: 
+    if args.toggle_case:
         print("This feature has been depricated. By default all calculations ignore case.  Use -s to consider case.")
     fc.ignore_case = not args.case_sensitive
     fc.ignorechars = args.exclude
@@ -202,5 +200,3 @@ if __name__ == "__main__":
                 fc.tally_str(eachline.decode("latin1"))
     if args.measure: print(fc.probability(args.measure))
     fc.save(args.freqtable)
-
-
