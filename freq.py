8246c416dcd6523f955518460129277f2cb57d53
MarkBaggett <lo127001@gmail.com>
2020-07-18 18:37:32 -0700

    Because @webbreacher

--- freq.py
+++ freq.py
@@ -372,7 +372,7 @@ if __name__ == "__main__":
     if args.bulk:
         with open(args.bulk,"rt") as filehandle:
             for eachline in filehandle:
-                print(fc.probability(eachline))
+                print(fc.probability(eachline),"-",eachline)
                 
     fc.save(args.freqtable)
 1
